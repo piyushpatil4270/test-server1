cdac0064d047d955bbc856afd8626142b1ed0ea2
Piyush <piyushpatil4270@gmail.com>
2024-03-05 14:55:10 +0530

    cmmt 5

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 
 let port = 5200
 
+app.get("/",(req,res)=>{
+    res.send("this is test server 1")
+})
 
 app.listen(port,()=>{
     console.log(`Server started on port ${port}...`)
